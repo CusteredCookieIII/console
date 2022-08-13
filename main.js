0c5357e4368dc99f342169a550a0c4cf0c5f85f4
CusteredCookieIII <105683628+CusteredCookieIII@users.noreply.github.com>
2022-08-13 12:55:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Blah blah blah blah blah blah blah");
+
+var first="Mason ";
+console.log(first);
+var Middle="Alexander ";
+console.log(Middle);
+var fullname=first.concat(Middle);
+console.log(fullname);
+var fullname1=fullname.replace("Alexander","Zenteno");
+console.log(fullname1);
+var uppercase=fullname1.toUpperCase();
+console.log(uppercase);
+var lowercauser=fullname1.toLowerCase();
+console.log(lowercauser);
+var Zenteno21=Math.sqrt(169);
+console.log(Zenteno21);
+var Zenteno12=Math.pow(10,2);
+console.log(Zenteno12);
